66c959dd1e415a8d746c432f95ba9e4e1d55d174
jaimezzapata <zapataval2304@gmail.com>
2024-10-10 13:51:26 -0500

    Introducción a JavaScript: generalidades de datos y variables

---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log("Hello World!!!") 
+// console.log('Hello World!!!');
+
+
+// prompt('Ingrese cualquier cosa')
+
+/* Variables */
+
+// var nombre = 'Jaime Zapata'
+// console.log(nombre)
+// var nombre = prompt('Ingrese el nombre de alguien: ')
+// console.log(nombre)
+
+/*  */
+
+// var numeroUno = parseInt(prompt('Ingrese un numero: '))
+// var numeroDos = parseInt(prompt('Ingrese un numero: '))
+// console.log(numeroUno + numeroDos)
+
+// console.log('2' + 2 + '2')
+// console.log((2 + 2) + '2')
+// console.log((2 + 2) - '2')
+// console.log('2' + '2' - '2')
+
+/* Tipos de datos */
